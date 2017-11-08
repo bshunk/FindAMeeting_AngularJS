42fb7ad6aff794f620bc88c2f7d38e91ce7e02f4
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-11-08 09:35:51 -0600

    fix bug so that day, time, and city all work correctly and display dynamically based upon user selection

--- app/factories/meetingsFactory.js
+++ app/factories/meetingsFactory.js
@@ -2,9 +2,11 @@
 
 app.factory('MeetingsFactory', function($q, $http) {
   
-  let getMeetings = (day) => {
+  let getMeetings = (day, time, city) => {
     console.log("DAY EQUALS", day);
-    return $http.get(`http://localhost:8000/api/v1/meetings/${day}`)
+    console.log("TIME EQUALS", time);
+    console.log("CITY EQUALS", city);
+    return $http.get(`http://localhost:8000/api/v1/meetings/${day}/${time}/${city}`)
   };
   console.log("get meetings equals", getMeetings);
   return { getMeetings };
